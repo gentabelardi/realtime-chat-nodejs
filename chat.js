527ab3b471a27ab1a3eaa544e6e82d7bf1d262ab
gentabelardi <78941367+gentabelardi@users.noreply.github.com>
2024-01-20 18:57:38 +0700

    bug: code improvements

--- chat.js
+++ chat.js
@@ -10,25 +10,19 @@ const MAX_PARTICIPANTS = 5;
 const MATCHMAKING_TIMEOUT = 60 * 1000;
 
 io.on('connection', socket => {
+  console.log(`User ${socket.id} connected`);
   let room;
   let timeout;
 
-  let participants = [];
-  // When a user clicks "Find" button
   socket.on('findMatch', (data) => {
-    // Check if there is an existing room for the given topic
-    for (let index in rooms) {
-      if (rooms[index].topic === data && !rooms[index].full) {
-        participants = io.sockets.adapter.rooms.get(index);
-        if (participants?.size < MAX_PARTICIPANTS) {
-          room = index;
-          break;
-        } else {
-          participants = [];
-        }
+    for (const [index, roomObj] of Object.entries(rooms)) {
+      const { topic, full, participants } = roomObj;
+      if (topic === data && !full && participants.size < MAX_PARTICIPANTS) {
+        room = index;
+        break;
       }
     }
-    // Create a new room if no existing room is available
+
     if (!room) {
       room = socket.id;
       rooms[room] = {
@@ -40,30 +34,26 @@ io.on('connection', socket => {
       };
     }
 
-    // Add the user to the room
     socket.join(room);
-    rooms[room]?.participants.add(socket.id);
+    rooms[room].participants.add(socket.id);
 
-    // Notify other participants that a new user has joined
-    socket.nsp.in(room).emit('userJoined', rooms[room]?.participants?.size)
+    io.to(room).emit('userJoined', rooms[room].participants.size);
 
-    if (rooms[room]?.participants?.size === MAX_PARTICIPANTS) {
+    if (rooms[room].participants.size === MAX_PARTICIPANTS) {
       rooms[room].full = true;
     }
 
     timeout = setTimeout(() => {
-
-      if (rooms[room]?.participants?.size < MIN_PARTICIPANTS) {
+      if (rooms[room].participants.size < MIN_PARTICIPANTS) {
         io.to(room).emit('matchmakingCancelled', "Matchmaking Cancelled");
         delete rooms[room];
         socket.leave(room);
         room = "";
-        participants = []
       } else {
         const chatRoom = `${room}`;
         io.to(room).emit('matchFound', chatRoom);
         io.sockets.adapter.rooms.set(chatRoom, new Set());
-        rooms[room]?.participants.forEach((participant) => {
+        rooms[room].participants.forEach((participant) => {
           io.sockets.sockets.get(participant).join(chatRoom);
           io.sockets.adapter.rooms.get(chatRoom).add(participant);
         });
@@ -71,58 +61,46 @@ io.on('connection', socket => {
     }, MATCHMAKING_TIMEOUT);
   });
 
-
   socket.on('joinRoom', ({ roomId, userId }) => {
-    if (Object.keys(rooms).length === 0) {
-      console.log("room kosong")
+    const targetRoom = rooms[roomId];
+    if (targetRoom) {
+      targetRoom.users.push(socket.id);
+      room = roomId;
+      socket.join(roomId);
+      console.log(`User ${socket.id} joined room ${roomId}`);
     } else {
-      for (let index in rooms) {
-        if (rooms[index].id === roomId) {
-          console.log("room joined")
-          if (!rooms[roomId].users) {
-            rooms[roomId].users = {};
-          }
-          rooms[roomId].users.push(socket.id)
-          console.log(rooms[roomId].users)
-          room = roomId;
-          socket.join(roomId);
-        } else {
-          console.log("room not found")
-        }
-      }
+      console.log(`Room ${roomId} not found`);
     }
   });
 
-  socket.emit('onlineUsers', rooms[room]?.users);
-
-
   socket.on('sendMessage', ({ roomId, newChat }) => {
-    io.to(roomId).emit('receiveMessage', { username: socket.username, newChat });
+    io.to(roomId).emit('receiveMessage', { username: socket.id, newChat });
   });
 
   socket.on('leave', ({ roomId }) => {
-    if (roomId) {
-      const roomParticipants = io.sockets.adapter.rooms.get(roomId);
-      if (roomParticipants?.size === 0) {
-        delete rooms[roomId];
-        console.log('room deleted');
-      } else {
-        console.log("roomParticipants: ", roomParticipants)
-        console.log('user leave');
-        socket.to(room).emit('user left', socket.id);
-      }
+    const roomParticipants = io.sockets.adapter.rooms.get(roomId);
+    if (roomParticipants?.size === 0) {
+      delete rooms[roomId];
+      console.log(`Room ${roomId} deleted`);
+    } else {
+      console.log(`User ${socket.id} left room ${roomId}`);
+      socket.to(roomId).emit('userLeft', socket.id);
     }
     clearTimeout(timeout);
   });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log(`User ${socket.id} disconnected`);
     if (room) {
       socket.to(room).emit('userLeft', socket.id);
-      delete rooms[room].users[socket.id];
+      rooms[room].users = rooms[room].users.filter(user => user !== socket.id);
       io.to(room).emit('offlineUser', rooms[room].users);
       room = "";
     }
     clearTimeout(timeout);
+  });
+
+  socket.on('error', (err) => {
+    console.error(`Socket error on user ${socket.id}: ${err}`);
   });
 });
